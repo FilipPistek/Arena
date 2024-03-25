ffdd6a88c65585ac2518cec8fdc9a1e9b062e1ef
Filip Pištěk <pistek@SPSEJECNANET.CZ>
2024-03-25 10:01:09 +0100

    Initial commit

---
+++ src/Class.java
@@ -0,0 +1,14 @@
+public class Class extends Player {
+
+    public static Class warrior() {
+        return new Class();
+    }
+
+    public static Class mage() {
+        return new Class();
+    }
+
+    public static Class ranger() {
+        return new Class();
+    }
+}
---
+++ src/Race.java
@@ -0,0 +1,27 @@
+public class Race extends Player {
+
+    public static Race human() {
+
+        return new Race();
+    }
+
+    public static Race elf() {
+        return new Race();
+    }
+
+    public static Race dwarf() {
+        return new Race();
+    }
+
+    public static Race troll() {
+        return new Race();
+    }
+
+    public static Race darkElf() {
+        return new Race();
+    }
+
+    public static Race undead() {
+        return new Race();
+    }
+}
